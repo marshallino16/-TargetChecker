ab6c1e4669ba25562c6cf78554b3a05908badef6
Anthony Fernandez <anthony@pictarine.com>
2017-02-01 20:17:11 +0100

    Initial Commit

---
+++ 🎯TargetsChecker/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  🎯TargetsChecker
+//
+//  Created by Junior on 01/02/2017.
+//  Copyright © 2017 Anthony Fernandez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
